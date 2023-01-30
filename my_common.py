1aeeb775ee9e6759da9a9660ae6dbe27c4941fc8
Dvin <dvyu@yandex.ru>
2023-01-30 13:49:06 +0500

    refactor some code and bug fixes

--- my_common.py
+++ my_common.py
@@ -6,14 +6,14 @@ def run_shell_command(command, output_flag):
     if output_flag == "Output=False":
         command_returned_value = os.system(command)
         if os.getenv('LOG') == 'DEBUG':
-            print('command_returned_value:', command_returned_value)
+            print('run_shell_command command_returned_value:', command_returned_value)
         return command_returned_value
     else:
         cmd_pipe = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         if os.getenv('LOG') == 'DEBUG':
-            print('SHELL run command:', command)
+            print('run_shell_command SHELL run command:', command)
         for command_response_line in cmd_pipe.stdout.readlines():
-            print('SHELL:', to_str(command_response_line))
+            print('run_shell_command SHELL:', to_str(command_response_line))
 
 # Convert from byte string to character Unicode string
 def to_str(byte_string):
@@ -36,6 +36,33 @@ def initialize_array(elements_cont):
     return new_array
 
 # Append string to file
-def add_string_to_gile(file, string):
+def add_string_to_file(file, string):
     with open(file, "a") as myfile:
         myfile.write(string)
+
+# Check if file extension allowed
+def is_extension_allowed(file_name):
+    allowed_extensions = ['.yaml', '.yml']
+    for extension_item in allowed_extensions:
+        extension = extension_item.lower()
+        file_name_string = to_str(file_name).lower()
+        if file_name_string.endswith(extension):
+            if os.getenv('LOG') == 'DEBUG':
+                print('is_extension_allowed found extension:', extension)
+            return True
+    return False
+
+# Check if file path allowed
+def is_path_allowed(file_name):
+    allowed_path = ['kubernetes']
+    #for path_item in allowed_path:
+    #    path = path_item.lower()
+    #    print('path:', path)
+    #    file_name_string = to_str(file_name).lower()
+    #    print('file_name_string:', file_name_string)
+    #    if file_name_string.startswith(path):
+    #        if os.getenv('LOG') == 'DEBUG':
+    #            print('found path:', path)
+    #            return True
+    #return False
+    return True
--- my_github.py
+++ my_github.py
@@ -1,8 +1,9 @@
 import os
+import re
 import requests
 
 # Get commit hash id by number
-def get_git_commit_hash_by_number(gh_token, gh_url, gh_pr_commit_number):
+def get_gh_commit_hash_by_number(gh_token, gh_url, gh_pr_commit_number):
     headers = {
         'Authorization': 'Bearer ' + str(gh_token),
         'Accept': 'application / vnd.github + json',
@@ -12,8 +13,8 @@ def get_git_commit_hash_by_number(gh_token, gh_url, gh_pr_commit_number):
     gh_response = requests.get(gh_url_with_page, headers=headers)
     gh_response_json = gh_response.json()
     if os.getenv('LOG') == 'DEBUG':
-        print('gh_url_with_page:', gh_url_with_page)
-        print('gh_response_json:', gh_response_json)
+        print('get_gh_commit_hash_by_number gh_url_with_page:', gh_url_with_page)
+        print('get_gh_commit_hash_by_number gh_response_json:', gh_response_json)
 
     return gh_response_json[0]["sha"]
 
@@ -28,32 +29,96 @@ def add_gh_pr_comment(gh_token, gh_url, gh_comment_body):
     gh_response = requests.post(gh_url, data=gh_comment, headers=headers)
     gh_response_json = gh_response.json()
     if os.getenv('LOG') == 'DEBUG':
-        print('gh_response_json:', gh_response_json)
+        print('add_gh_pr_comment gh_response_json:', gh_response_json)
 
+# Get commit comment message by pool request number
+def get_gh_pr_comment_by_pr_id(gh_token, gh_template_url, gh_pr_number):
+    gh_pr_comment = ''
+    headers = {
+        'Authorization': 'Bearer ' + str(gh_token),
+        'Accept': 'application / vnd.github + json',
+        'X-GitHub-Api-Version': '2022-11-28'
+            }
+    gh_url_array = gh_template_url.split("/")
+    gh_url_protocol = gh_url_array[0]
+    gh_url_server = gh_url_array[2]
+    gh_url_owner = gh_url_array[4]
+    gh_url_repo = gh_url_array[5]
+    gh_comments_url = gh_url_protocol + "//" + gh_url_server + '/repos/' + gh_url_owner + '/' + gh_url_repo \
+                      + '/pulls/' + str(gh_pr_number)
+    gh_response = requests.get(gh_comments_url, headers=headers)
+    gh_response_json = gh_response.json()
+    if gh_response_json["body"] != None:
+        gh_pr_comment = gh_response_json["body"].split("\r\n")
 
-def get_gh_pr_comment(gh_full_json):
     if os.getenv('LOG') == 'DEBUG':
-        print('gh_full_json["event"]["pull_request"]["body"]:', gh_full_json["event"]["pull_request"]["body"])
-    if gh_full_json["event"]["pull_request"]["body"] != None:
-         gh_pr_comment = gh_full_json["event"]["pull_request"]["body"].split("\r\n")
-         if os.getenv('LOG') == 'DEBUG':
-             print('gh_full_json["event"]["pull_request"]["body"]:', gh_full_json["event"]["pull_request"]["body"])
-         return gh_pr_comment
+        print('gh_url:', gh_template_url)
+        print('gh_url_array:', gh_url_array)
+        print('gh_url_protocol:', gh_url_protocol)
+        print('gh_url_server:', gh_url_server)
+        print('gh_url_owner:', gh_url_owner)
+        print('gh_url_repo:', gh_url_repo)
+        print('gh_comments_url:', gh_comments_url)
+        print('gh_response:', gh_response)
+        print('gh_response_json:', gh_response_json)
+        print('gh_pr_comment:', gh_pr_comment)
 
+    return gh_pr_comment
+
+# Combine comments_url from template url
+def create_comments_url_by_pr_id(gh_template_url, gh_pr_number):
+    gh_url_array = gh_template_url.split("/")
+    gh_url_protocol = gh_url_array[0]
+    gh_url_server = gh_url_array[2]
+    gh_url_owner = gh_url_array[4]
+    gh_url_repo = gh_url_array[5]
+    gh_comments_url = gh_url_protocol + "//" + gh_url_server + '/repos/' + gh_url_owner + '/' + gh_url_repo \
+                      + '/issues/' + str(gh_pr_number) + '/comments'
+
+    return gh_comments_url
+
+# Get pull request comment form 'github' environment variable
+def get_gh_pr_comment_from_env(gh_full_json):
+    gh_pr_comment = ''
+    if os.getenv('LOG') == 'DEBUG':
+        print('get_gh_pr_comment gh_full_json["event"]["pull_request"]["body"]:',
+              gh_full_json["event"]["pull_request"]["body"])
+    if gh_full_json["event"]["pull_request"]["body"] != None:
+        gh_pr_comment = gh_full_json["event"]["pull_request"]["body"].split("\r\n")
+
+    return gh_pr_comment
+
+# Get pull request number from 'github' environment variable message key
+def get_gh_pr_number_from_env(gh_full_json):
+    gh_pr_number = ''
+    if os.getenv('LOG') == 'DEBUG':
+        print('gh_full_json["event"]["head_commit"]["message"]:', gh_full_json["event"]["head_commit"]["message"])
+    gh_event_message_regexp = re.search('^Merge pull request #([0-9]+) from .*',
+                                        gh_full_json["event"]["head_commit"]["message"])
+    if os.getenv('LOG') == 'DEBUG':
+        print('gh_event_message_regexp:', gh_event_message_regexp)
+    if gh_event_message_regexp != None:
+        gh_pr_number = gh_event_message_regexp.group(1)
+        if os.getenv('LOG') == 'DEBUG':
+            print('gh_pr_number:', gh_pr_number)
+
+    return gh_pr_number
+
+# Get deployment order list form pull request comment string
 def get_order_list_from_comment(gh_pr_comment):
     order_list = {}
     found_order_list = 0
     count = 0
     for pr_comment_line in gh_pr_comment:
         if os.getenv('LOG') == 'DEBUG':
-            print('pr_comment_line:', pr_comment_line)
+            print('get_order_list_from_comment pr_comment_line:', pr_comment_line)
         if pr_comment_line == '' and found_order_list == 1:
             if os.getenv('LOG') == 'DEBUG':
-                print("empty string found...")
+                print("get_order_list_from_comment empty string found...")
             break
         if "```" in str(pr_comment_line) and found_order_list == 1:
             if os.getenv('LOG') == 'DEBUG':
-                print("end of comment string found...")
+                print("get_order_list_from_comment end of comment string found...")
             break
         if found_order_list == 1:
             if "group:" in str(pr_comment_line):
@@ -61,17 +126,18 @@ def get_order_list_from_comment(gh_pr_comment):
                 count += 1
             else:
                 if os.getenv('LOG') == 'DEBUG':
-                    print('not valid pr_comment_line:', pr_comment_line)
+                    print('get_order_list_from_comment not valid pr_comment_line:', pr_comment_line)
             if os.getenv('LOG') == 'DEBUG':
-                print('count:', count)
-                print('pr_comment_line:', pr_comment_line)
-                print('order_list[pr_comment_line]:', order_list[pr_comment_line])
+                print('get_order_list_from_comment count:', count)
+                print('get_order_list_from_comment pr_comment_line:', pr_comment_line)
+                #print('order_list[pr_comment_line]:', order_list[pr_comment_line])
         if "~deployment-order" in str(pr_comment_line):
             print("deployment-order detected...")
             found_order_list = 1
     if found_order_list:
         return order_list
 
+# Get deployment order list form file
 def get_order_list_from_file(deployment_order_file):
     order_list = {}
     count = 0
@@ -83,21 +149,21 @@ def get_order_list_from_file(deployment_order_file):
             count += 1
         else:
             if os.getenv('LOG') == 'DEBUG':
-                print('not valit group_file_line.strip():', group_file_line.strip())
+                print('get_order_list_from_file not valit group_file_line.strip():', group_file_line.strip())
         if os.getenv('LOG') == 'DEBUG':
-            print('count:', count)
-            print('group_file_line.strip():', group_file_line.strip())
-            print('order_list[group_file_line.strip()]:', order_list[group_file_line.strip()])
+            print('get_order_list_from_file count:', count)
+            print('get_order_list_from_file group_file_line.strip():', group_file_line.strip())
+            print('get_order_list_from_file order_list[group_file_line.strip()]:', order_list[group_file_line.strip()])
     return order_list
 
+# Create new array of deployment groups from dictionary with 'group name' keys
 def convert_order_list(deployment_order_names):
     if os.getenv('LOG') == 'DEBUG':
-        print('len(deployment_order_names):', len(deployment_order_names))
+        print('convert_order_list len(deployment_order_names):', len(deployment_order_names))
     converted_array = [None] * len(deployment_order_names)
     for deployment_order_name_key, deployment_order_name_value in deployment_order_names.items():
         if os.getenv('LOG') == 'DEBUG':
-            print('deployment_order_name_key:', deployment_order_name_key)
-            print('deployment_order_name_value:', deployment_order_name_value)
+            print('convert_order_list deployment_order_name_key:', deployment_order_name_key)
+            print('convert_order_list deployment_order_name_value:', deployment_order_name_value)
         converted_array[deployment_order_name_value] = deployment_order_name_key
     return converted_array
-
